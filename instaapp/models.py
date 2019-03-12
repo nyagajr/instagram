2c7222c84919fadb4b4b2da301beeecf63dc34d2
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-12 11:11:18 +0300

    new models

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length = 30)
+    username = models.CharField(max_length = 30)
+    # image_location = models.ForeignKey(Location)
+    image_path = models.ImageField(upload_to = 'gallery/')
+    image_description = models.TextField()
+
+class Comment(models.Model):
+    comments = models.CharField(max_length =30)
