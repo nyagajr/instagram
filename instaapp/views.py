79dc752e4f20cae247abc32fd046917e99adc49d
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-12 11:36:54 +0300

    creating view for signup page

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import NewsLetterForm
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def signup(request):
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'signup.html')
+
+
+
+
+
+# def welcome(request):
+    # return render(request, 'welcome.html')
