30d55ba4bce38a4539c83c44991421d1c47b4e9d
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-13 08:53:22 +0300

    signup form

--- instaapp/views.py
+++ instaapp/views.py
@@ -15,7 +15,18 @@ def signup(request):
 
 
 
-
+# def news_today(request):
+#     if request.method == 'POST':
+#         form = NewsLetterForm(request.POST)
+#         if form.is_valid():
+#             name = form.cleaned_data['your_name']
+#             email = form.cleaned_data['email']
+#             recipient = NewsLetterRecipients(name = name,email =email)
+#             recipient.save()
+#             HttpResponseRedirect('news_today')
+#     else:
+#         form = NewsLetterForm()
+#     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 # def welcome(request):
     # return render(request, 'welcome.html')
--- instagram/urls.py
+++ instagram/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    # url(r'', include('registration.backends.simple.urls')),
     url(r'', include('instaapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
